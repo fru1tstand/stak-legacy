3c92de696fbec33f7d5a9abbfae26e026fcfad44
Kodlee Yin <kodlee@kodleeshare.net>
2015-02-12 13:55:20 -0800

    Completed template shell

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Fru1tMe - Homework Thing</title>
+		<meta charset="UTF-8" />
+		
+		<link href="/.site/css/compiled/global.css" rel="stylesheet" type="text/css">
+		
+	</head>
+	
+	<body>
+		<div id="nav-menu">
+			This is some text
+		</div>
+		
+		<div id="content">
+			<div>
+				this is tasklist
+			</div>
+			<div>
+				this is more
+			</div>
+		</div>
+		
+	</body>
+</html>
