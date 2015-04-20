00afed46e29a511871d0174a3518fc996add7387
Kodlee Yin <kodlee@kodleeshare.net>
2015-04-20 09:57:28 -0700

    Implemented Mock version of UserData

--- .site/phptests/stak/MockStakModule.php
+++ .site/phptests/stak/MockStakModule.php
@@ -6,11 +6,14 @@ use stak\base\Tag;
 use stak\base\MockTag;
 use stak\base\Task;
 use stak\base\MockTask;
+use stak\base\UserData;
+use stak\base\MockUserData;
 
 class MockStakModule extends AbstractModule {
 	public function configure() {
 		self::bind(StakSettings::class)->asSingletonTo(MockStakSettings::class);
 		self::bind(Tag::class)->to(MockTag::class);
 		self::bind(Task::class)->to(MockTask::class);
+		self::bind(UserData::class)->to(MockUserData::class);
 	}
 }
