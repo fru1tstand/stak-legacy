eb5a96b8a374c4d857c9246fc0a8e45848d3aed8
Kodlee Yin <kodlee@kodleeshare.net>
2015-04-20 09:57:11 -0700

    Added use of TaskFilter

--- .site/php/stak/base/UserData.php
+++ .site/php/stak/base/UserData.php
@@ -1,19 +1,17 @@
 <?php
 namespace stak\base;
 require_once $_SERVER["DOCUMENT_ROOT"] . "/.site/php/stak/Autoload.php";
+use stak\base\userdata\TaskFilter;
 
 /**
  * Provides an interface to access user data like stored tasks, tags, settings, account info, etc.
  * @package stak
  */
 interface UserData {
-
-	// Authentication
-
-
-	// Tag-specific commands
-
-
-	// Task-specific commands
-	public function getAllTasks();
+	/**
+	 * Gets tasks with the given filter. Pass null to get all tasks.
+	 * @param TaskFilter $filter
+	 * @return mixed
+	 */
+	public function getTasks(TaskFilter $filter = null);
 }
