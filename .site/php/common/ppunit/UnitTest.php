b30677385c4a3b4d9c1a8e0202e3d1d4d9c5262e
Kodlee Yin <kodlee@kodleeshare.net>
2015-04-25 00:24:42 -0700

    Added assert comments; Changed test detection via phpDoc

--- .site/php/common/ppunit/UnitTest.php
+++ .site/php/common/ppunit/UnitTest.php
@@ -18,7 +18,7 @@ abstract class UnitTest {
 		$tests = array();
 		$reflectionObject = new ReflectionObject($unitTest);
 		foreach ($reflectionObject->getMethods() as $method) {
-			if (preg_match("/^(Test).+$/", $method->getName()))
+			if (strpos($method->getDocComment(), "@test"))
 				$tests[] = $method;
 		}
 
@@ -27,18 +27,27 @@ abstract class UnitTest {
 		require str_replace("\\", "/", realpath(dirname(__FILE__))) . "/html/TestPageContent.php";
 	}
 
-	protected static function assertTrue($logic) {
+	protected static function assertTrue($logic, $comment = null) {
 		if (!$logic)
 			throw new Exception("Assertion true failed");
+
+		if (!is_null($comment))
+			echo "<div>True assertion passed: ", $comment, "</div>";
 	}
 
-	protected static function assertFalse($logic) {
+	protected static function assertFalse($logic, $comment = null) {
 		if ($logic)
 			throw new Exception("Assertion false failed");
+
+		if (!is_null($comment))
+			echo "<div>False assertion passed: ", $comment, "</div>";
 	}
 
-	protected static function assertEqual($obj1, $obj2) {
+	protected static function assertEqual($obj1, $obj2, $comment = null) {
 		if ($obj1 !== $obj2)
 			throw new Exception("Assertion equal failed");
+
+		if (!is_null($comment))
+			echo "<div>Equality assertion passed: ", $comment, "</div>";
 	}
 }
