79fa3e0e34a3767a6d25d8afd71db3fb85e5b088
Kodlee Yin <kodlee@kodleeshare.net>
2015-05-01 10:39:44 -0700

    Implemented autogenerated view

--- .site/php/stak/template/content/listpage/SingleTagView.php
+++ .site/php/stak/template/content/listpage/SingleTagView.php
@@ -11,12 +11,13 @@ $tagGroup = ListProcessor::getTagGroup();
 	<input type="radio" name="tasklist-tag-select" class="tasklist-tag-option-controller" id="tc-master" checked="checked" />
 	<div class="tasklist-tag-container">
 		<?php
-		foreach ($tagGroup->getTagContainers() as $tc) {
-			$tagHash = $tc->getTag()->getHash();
-			$tagName = htmlspecialchars($tc->getTag()->getName());
-			$tagColor = $tc->getTag()->getColor();
+		foreach ($tagGroup->getTagContainers() as $tagContain) {
+			$tagHash = $tagContain->getTag()->getHash();
+			$tagName = htmlspecialchars($tagContain->getTag()->getName());
+			$tagColor = $tagContain->getTag()->getColor();
 
 			echo <<<HTML
+
 		<label class="tasklist-tag-container-toggle" for="tc-$tagHash">
 			<span class="tasklist-tag">
 				<span class="tag" style="background-color: #$tagColor"></span>
@@ -24,274 +25,122 @@ $tagGroup = ListProcessor::getTagGroup();
 				<span class="title">$tagName</span>
 			</span>
 		</label>
+
 HTML;
 		}
 		?>
 	</div>
 
-	<input type="radio" name="tasklist-tag-select" class="tasklist-tag-option-controller" id="tc-hash1" />
+	<?php
+	foreach ($tagGroup->getTagContainers() as $tagContain) {
+		$tagHash = $tagContain->getTag()->getHash();
+		$tagName = htmlspecialchars($tagContain->getTag()->getName());
+		$tagColor = $tagContain->getTag()->getColor();
+        $slideFrame = 0;
+
+		// Tag container html
+		echo <<<HTML
+
+	<input type="radio" name="tasklist-tag-select" class="tasklist-tag-option-controller" id="tc-$tagHash" />
 	<div class="tasklist-tag-container">
-		<label class="tasklist-tag-container-toggle" for="tc-master">
+		<label class="tasklist-tag-container-toggle slide-in slide-frame-0" for="tc-master">
 				<span class="tasklist-tag">
-					<span class="tag" style="background-color:#99F"></span>
+					<span class="tag" style="background-color: #$tagColor;"></span>
 					<span class="options-toggle"></span>
-					<span class="title">School tag that's very long and stuff but doesn't quite make it</span>
+					<span class="title">$tagName</span>
 				</span>
 		</label>
-
 		<div class="tasklist-timescope-group">
 
-			<div class="tasklist-timescope-container">
-				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash1" />
+HTML;
 
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash1">
-						<span class="tasklist-timescope">
-							<span class="count">1</span>
-							<span class="visibility-toggle"></span>
-							<span class="name">Overdue</span>
-						</span>
-				</label>
+		// timescope group html
+		foreach ($tagContain->getTimescopeGroup()->getTimescopeContainers() as $tsContain) {
+			$tsContainHash = ListProcessor::getTimescopeTagHash($tsContain->getTimescope(),
+							$tagContain->getTag());
+            $tsContainIncompleteCount = $tsContain->countIncompleteTasks();
+            $tsContainName = $tsContain->getTimescope()->getName();
+            $slideFrame++;
 
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #99F"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Some Title that's quite long and annoyiong to deal with even with the special html</span>
-								<span class="hierarchy">An<i></i>Example</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-
-			</div> <!-- .tasklist-timescope-container -->
+			// Timescope container html
+			echo <<<HTML
 
 			<div class="tasklist-timescope-container">
 				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash2" />
+				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-$tsContainHash" />
 
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash2">
+				<label class="tasklist-timescope-container-toggle slide-in slide-frame-$slideFrame" for="tsc-$tsContainHash">
 						<span class="tasklist-timescope">
-							<span class="count">2</span>
+							<span class="count">$tsContainIncompleteCount</span>
 							<span class="visibility-toggle"></span>
-							<span class="name">Today</span>
+							<span class="name">$tsContainName</span>
 						</span>
 				</label>
 
-				<div class="tasklist-task complete">
-					<div class="tag" style="border-color: #99F"></div>
+HTML;
+
+            // Task html
+            foreach ($tsContain->getTasks() as $task) {
+                $taskColor = $task->getPrimaryTag()->getColor();
+                $taskTitle = htmlspecialchars($task->getTitle());
+                $taskHierarchy = null;
+                if ($task->hasParent())
+                    $taskHierarchy = $task->getParent()->getTitle();
+
+                echo <<<HTML
+
+                <div class="tasklist-task">
+					<div class="tag" style="border-color: #$taskColor"></div>
 					<div class="tasklist-quickedit">
-						<a href="#"><i class="uncomplete"></i></a>
+
+HTML;
+                if (!$task->isComplete())
+                    echo '<a href="#"><i class="complete"></i></a>';
+                else
+                    echo '<a href="#"><i class="uncomplete"></i></a>';
+
+                echo <<<HTML
+
 						<a href="#"><i class="edit"></i></a>
 						<a href="#"><i class="delete"></i></a>
 					</div>
 					<label class="tasklist-task-toggle">
 						<span class="middler"></span>
 							<span class="middle">
-								<span class="title">Giant Music Project</span>
-								<span class="hierarchy hidden"></span>
+								<span class="title">$taskTitle</span>
+
+HTML;
+                if ($task->hasParent())
+                    echo '<span class="hierarchy">', $taskHierarchy, '</span>';
+
+                echo <<<HTML
+
 							</span>
 					</label>
 				</div><!-- .tasklist-task -->
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #99F"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Physics Webassign 24 - 40</span>
-								<span class="hierarchy">Physics<i></i>Homework<i></i>Webassign</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
+
+HTML;
+
+            }
+
+			// End timescope container html
+			echo <<<HTML
+
 			</div> <!-- .tasklist-timescope-container -->
+
+HTML;
+		}
+
+		// End tag container html
+		echo <<<HTML
+
 		</div> <!-- .tasklist-timescope-group -->
 	</div> <!-- .tasklist-tag-container -->
 
-	<input type="radio" name="tasklist-tag-select" class="tasklist-tag-option-controller" id="tc-hash2" />
-	<div class="tasklist-tag-container">
-		<label class="tasklist-tag-container-toggle" for="tc-master">
-				<span class="tasklist-tag">
-					<span class="tag" style="background-color:#9FF"></span>
-					<span class="options-toggle"></span>
-					<span class="title">Important</span>
-				</span>
-		</label>
+HTML;
 
-		<div class="tasklist-timescope-group">
 
-			<div class="tasklist-timescope-container">
-				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash3" />
+	}
+	?>
 
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash3">
-						<span class="tasklist-timescope">
-							<span class="count">1</span>
-							<span class="visibility-toggle"></span>
-							<span class="name">Overdue</span>
-						</span>
-				</label>
-
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #9FF"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Some Title that's quite long and annoyiong to deal with even with the special html</span>
-								<span class="hierarchy">An<i></i>Example</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-
-			</div> <!-- .tasklist-timescope-container -->
-
-			<div class="tasklist-timescope-container">
-				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash4" />
-
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash4">
-						<span class="tasklist-timescope">
-							<span class="count">2</span>
-							<span class="visibility-toggle"></span>
-							<span class="name">Today</span>
-						</span>
-				</label>
-
-				<div class="tasklist-task complete">
-					<div class="tag" style="border-color: #9FF"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="uncomplete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Giant Music Project</span>
-								<span class="hierarchy hidden"></span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #9FF"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Physics Webassign 24 - 40</span>
-								<span class="hierarchy">Physics<i></i>Homework<i></i>Webassign</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-
-			</div> <!-- .tasklist-timescope-container -->
-		</div> <!-- .tasklist-timescope-group -->
-	</div> <!-- .tasklist-tag-container -->
-
-	<input type="radio" name="tasklist-tag-select" class="tasklist-tag-option-controller" id="tc-hash3" />
-	<div class="tasklist-tag-container">
-		<label class="tasklist-tag-container-toggle" for="tc-master">
-				<span class="tasklist-tag">
-					<span class="tag" style="background-color:#0F0"></span>
-					<span class="options-toggle"></span>
-					<span class="title">Projects</span>
-				</span>
-		</label>
-
-		<div class="tasklist-timescope-group">
-
-			<div class="tasklist-timescope-container">
-				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash5" />
-
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash5">
-						<span class="tasklist-timescope">
-							<span class="count">1</span>
-							<span class="visibility-toggle"></span>
-							<span class="name">Overdue</span>
-						</span>
-				</label>
-
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #0F0"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Some Title that's quite long and annoyiong to deal with even with the special html</span>
-								<span class="hierarchy">An<i></i>Example</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-
-			</div> <!-- .tasklist-timescope-container -->
-
-			<div class="tasklist-timescope-container">
-				<!-- Controls this timescope for the tag -->
-				<input type="checkbox" class="tasklist-timescope-container-controller" id="tsc-hash6" />
-
-				<label class="tasklist-timescope-container-toggle" for="tsc-hash6">
-						<span class="tasklist-timescope">
-							<span class="count">2</span>
-							<span class="visibility-toggle"></span>
-							<span class="name">Today</span>
-						</span>
-				</label>
-
-				<div class="tasklist-task complete">
-					<div class="tag" style="border-color: #0F0"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="uncomplete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Giant Music Project</span>
-								<span class="hierarchy hidden"></span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-				<div class="tasklist-task">
-					<div class="tag" style="border-color: #0F0"></div>
-					<div class="tasklist-quickedit">
-						<a href="#"><i class="complete"></i></a>
-						<a href="#"><i class="edit"></i></a>
-						<a href="#"><i class="delete"></i></a>
-					</div>
-					<label class="tasklist-task-toggle">
-						<span class="middler"></span>
-							<span class="middle">
-								<span class="title">Physics Webassign 24 - 40</span>
-								<span class="hierarchy">Physics<i></i>Homework<i></i>Webassign</span>
-							</span>
-					</label>
-				</div><!-- .tasklist-task -->
-
-			</div> <!-- .tasklist-timescope-container -->
-		</div> <!-- .tasklist-timescope-group -->
-	</div> <!-- .tasklist-tag-container -->
 </div> <!-- .tasklist-viewstyle-container -->
