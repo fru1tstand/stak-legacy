dfd7e15fb6cb400b003d1bc48a7e19216b4f2bcd
Kodlee Yin <kodlee@kodleeshare.net>
2015-05-04 10:53:50 -0700

    Added basic use case asserts to compareChronological test

--- .site/phptests/stak/foundation/TimescopeTest.php
+++ .site/phptests/stak/foundation/TimescopeTest.php
@@ -158,14 +158,30 @@ class TimescopeTest extends UnitTest {
 		$tsNowToFuture = new MockTimescope("", 0, false, 0, true, false, false, true);
 		$tsNowToThisWeek = new MockTimescope("", 0, false, 7, false, false, false, true);
 
+		// Basic use case
+		$today = new MockTimescope("Today", 0, false, 1, false, false, false, true);
+		$thisWeek = new MockTimescope("This Week", 0, false, 7, false, false, true, true);
+		$overdue = new MockTimescope("Overdue", 0, true, 0, false, true, true, true);
+		$everythingElse =
+				new MockTimescope("Everything Else", 7, false, 0, true, true, true, false);
+
+		self::addMessage("Basic Constructs");
 		self::assertTrue($ts500to1000a->compareChronological($ts500to1000b) < 0, "Same range, sorts alphabetical, A comes before B");
 		self::assertTrue($ts500to1000b->compareChronological($ts500to1000a) > 0, "Same range, sorta alphabetical, B comes after A");
 		self::assertTrue($ts500to1000a->compareChronological($ts500to1001) < 0, "Same lower bound, shorter range comes before longer range");
 		self::assertTrue($ts500to1001->compareChronological($ts500to1000a) > 0, "Same lower bound, longer range comes after shorter range");
+		self::addMessage("Torture Tests");
 		self::assertTrue($tsNowToTomorrow->compareChronological($tsTodayToTomorrow) > 0, "Today starts before now");
 		self::assertTrue($tsNowToTomorrow->compareChronological($tsNowToFuture) < 0, "Same lower bound (now), finite length is less than infinite");
 		self::assertTrue($tsNowToFuture->compareChronological($tsTodayToTomorrow) > 0, "Lower bound takes precedence over length");
 		self::assertTrue($tsNowToTomorrow->compareChronological($tsNowToThisWeek) < 0, "Today (now) comes before this week (now)");
+		self::addMessage("Basic Use Case");
+		self::assertTrue($overdue->compareChronological($today) < 0, "Overdue is listed first (Compared today)");
+		self::assertTrue($overdue->compareChronological($thisWeek) < 0, "Overdue is listed first (Compared this week)");
+		self::assertTrue($overdue->compareChronological($everythingElse) < 0, "Overdue is listed first (Compared everything else)");
+		self::assertTrue($today->compareChronological($thisWeek) < 0, "Today is listed next (Compared this week)");
+		self::assertTrue($today->compareChronological($everythingElse) < 0, "Today is listed next (Compared everything else)");
+		self::assertTrue($thisWeek->compareChronological($everythingElse) < 0, "This week is listed next (Compared everything else)");
 	}
 }
 TestRunner::run(new TimescopeTest());
